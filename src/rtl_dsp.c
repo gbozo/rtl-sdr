8687bb15d3c5f101cc2fcf670ce0d3683877c6fc
rampage <4042540+gbozo@users.noreply.github.com>
2026-05-22 16:21:10 +0300

    rtl_dsp: replace CIC with polyphase FIR decimator
    Polyphase FIR is significantly more efficient than CIC for SDR
    use cases:
    - Only processes output-rate samples (saves factor of R)
    - Flat passband, sharp cutoff, proper anti-aliasing
    - No catastrophic gain buildup (CIC had R^3 gain)
    - Blackman-Harris windowed sinc filter, 128 taps
    - Per-phase circular buffers avoid O(P) memory shifts
    - Same DSP interface, drop-in replacement

--- src/rtl_dsp.c
+++ src/rtl_dsp.c
@@ -8,6 +8,8 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+#define MAX_TAPS 512
+
 struct rtlsdr_dsp {
 	uint64_t input_rate;
 	uint64_t output_rate;
@@ -18,14 +20,49 @@ struct rtlsdr_dsp {
 	double   phase;
 	double   phase_inc;
 
-	int      cic_stages;
-	int      cic_counter;
-	double  *int_i;
-	double  *int_q;
-	double  *comb_i;
-	double  *comb_q;
-	double   cic_gain;
+	int      num_taps;
+	int      taps_per_phase;
+	int      num_phases;
+	double  *coeffs;
+	double  *delay_i;
+	double  *delay_q;
+	int     *delay_pos;
+	int      comm_pos;
 };
+
+static double blackman_harris(int n, int N)
+{
+	double a0 = 0.35875;
+	double a1 = 0.48829;
+	double a2 = 0.14128;
+	double a3 = 0.01168;
+	double t = 2.0 * M_PI * n / (N - 1);
+	return a0 - a1 * cos(t) + a2 * cos(2 * t) - a3 * cos(3 * t);
+}
+
+static int design_lowpass(double *coeffs, int num_taps, double cutoff)
+{
+	int i, mid = (num_taps - 1) / 2;
+	double sum = 0.0;
+
+	for (i = 0; i < num_taps; i++) {
+		int k = i - mid;
+		double sinc;
+		if (k == 0)
+			sinc = 2.0 * cutoff;
+		else
+			sinc = sin(2.0 * M_PI * cutoff * k) / (M_PI * k);
+		coeffs[i] = sinc * blackman_harris(i, num_taps);
+		sum += coeffs[i];
+	}
+
+	if (sum != 0.0) {
+		for (i = 0; i < num_taps; i++)
+			coeffs[i] /= sum;
+	}
+
+	return 0;
+}
 
 struct rtlsdr_dsp *rtlsdr_dsp_create(uint64_t input_rate,
 				     uint64_t output_rate,
@@ -33,14 +70,17 @@ struct rtlsdr_dsp *rtlsdr_dsp_create(uint64_t input_rate,
 				     uint64_t freq_offset)
 {
 	struct rtlsdr_dsp *dsp;
-	int r;
+	int R, N_padded, i, j, p;
+	double cutoff, *tmp;
 
 	if (input_rate == 0 || output_rate == 0 || output_rate > input_rate)
 		return NULL;
 
-	r = (int)(input_rate / output_rate);
-	if (r < 2)
-		r = 1;
+	R = (int)(input_rate / output_rate);
+	if (R < 2)
+		R = 1;
+	if (R < 1)
+		R = 1;
 
 	dsp = (struct rtlsdr_dsp *)calloc(1, sizeof(*dsp));
 	if (!dsp)
@@ -48,38 +88,71 @@ struct rtlsdr_dsp *rtlsdr_dsp_create(uint64_t input_rate,
 
 	dsp->input_rate  = input_rate;
 	dsp->output_rate = output_rate;
-	dsp->decimation  = r;
+	dsp->decimation  = R;
 	dsp->bandwidth   = bandwidth;
 	dsp->freq_offset = freq_offset;
 
 	dsp->phase_inc = 2.0 * M_PI * (double)freq_offset / (double)input_rate;
 	dsp->phase = 0.0;
 
-	dsp->cic_stages = 3;
-	dsp->cic_counter = 0;
+	dsp->num_phases = R;
 
-	dsp->int_i  = (double *)calloc(dsp->cic_stages, sizeof(double));
-	dsp->int_q  = (double *)calloc(dsp->cic_stages, sizeof(double));
-	dsp->comb_i = (double *)calloc(dsp->cic_stages, sizeof(double));
-	dsp->comb_q = (double *)calloc(dsp->cic_stages, sizeof(double));
+	if (bandwidth == 0 || bandwidth >= output_rate)
+		cutoff = 0.45;
+	else
+		cutoff = (double)bandwidth * 0.5 / (double)input_rate;
 
-	if (!dsp->int_i || !dsp->int_q || !dsp->comb_i || !dsp->comb_q) {
-		free(dsp->int_i);  free(dsp->int_q);
-		free(dsp->comb_i); free(dsp->comb_q);
-		free(dsp);
-		return NULL;
-	}
+	if (cutoff <= 0.0 || cutoff >= 0.5)
+		cutoff = 0.45;
 
-	dsp->cic_gain = pow((double)r, (double)dsp->cic_stages);
+	dsp->num_taps = 128;
+	if (dsp->num_taps > MAX_TAPS)
+		dsp->num_taps = MAX_TAPS;
 
+	dsp->taps_per_phase = (dsp->num_taps + R - 1) / R;
+	if (dsp->taps_per_phase < 4)
+		dsp->taps_per_phase = 4;
+
+	N_padded = dsp->taps_per_phase * R;
+	if (N_padded > MAX_TAPS)
+		N_padded = MAX_TAPS;
+
+	tmp = (double *)calloc(N_padded, sizeof(double));
+	if (!tmp)
+		goto fail;
+	design_lowpass(tmp, dsp->num_taps, cutoff);
+
+	dsp->coeffs = (double *)calloc(N_padded, sizeof(double));
+	dsp->delay_i = (double *)calloc(N_padded, sizeof(double));
+	dsp->delay_q = (double *)calloc(N_padded, sizeof(double));
+	dsp->delay_pos = (int *)calloc(R, sizeof(int));
+	if (!dsp->coeffs || !dsp->delay_i || !dsp->delay_q || !dsp->delay_pos)
+		goto fail;
+
+	for (p = 0; p < R; p++)
+		for (j = 0; j < dsp->taps_per_phase; j++)
+			dsp->coeffs[p * dsp->taps_per_phase + j] = tmp[p + j * R];
+
+	free(tmp);
 	return dsp;
+
+fail:
+	free(tmp);
+	free(dsp->coeffs);
+	free(dsp->delay_i);
+	free(dsp->delay_q);
+	free(dsp->delay_pos);
+	free(dsp);
+	return NULL;
 }
 
 void rtlsdr_dsp_destroy(struct rtlsdr_dsp *dsp)
 {
 	if (dsp) {
-		free(dsp->int_i);  free(dsp->int_q);
-		free(dsp->comb_i); free(dsp->comb_q);
+		free(dsp->coeffs);
+		free(dsp->delay_i);
+		free(dsp->delay_q);
+		free(dsp->delay_pos);
 		free(dsp);
 	}
 }
@@ -89,23 +162,22 @@ int rtlsdr_dsp_process(struct rtlsdr_dsp *dsp,
 		       int16_t *output, size_t *output_len)
 {
 	size_t in_idx = 0, out_idx = 0;
-	double cos_phase, sin_phase;
-	double mix_i, mix_q;
-	double cic_i, cic_q;
-	int i;
+	int P = dsp->taps_per_phase;
+	int R = dsp->num_phases;
 
 	if (!dsp || !input || !output || !output_len)
 		return -1;
 
 	while (in_idx + 1 < input_len) {
-		cos_phase = cos(dsp->phase);
-		sin_phase = sin(dsp->phase);
+		double mix_i, mix_q, cos_ph, sin_ph;
+		int p;
 
-		mix_i = (double)input[in_idx] * cos_phase +
-			(double)input[in_idx + 1] * sin_phase;
-		mix_q = (double)input[in_idx + 1] * cos_phase -
-			(double)input[in_idx] * sin_phase;
-
+		cos_ph = cos(dsp->phase);
+		sin_ph = sin(dsp->phase);
+		mix_i = (double)input[in_idx] * cos_ph +
+			(double)input[in_idx + 1] * sin_ph;
+		mix_q = (double)input[in_idx + 1] * cos_ph -
+			(double)input[in_idx] * sin_ph;
 		in_idx += 2;
 
 		dsp->phase += dsp->phase_inc;
@@ -114,41 +186,39 @@ int rtlsdr_dsp_process(struct rtlsdr_dsp *dsp,
 		if (dsp->phase < 0.0)
 			dsp->phase += 2.0 * M_PI;
 
-		dsp->int_i[0] += mix_i;
-		dsp->int_q[0] += mix_q;
-		for (i = 1; i < dsp->cic_stages; i++) {
-			dsp->int_i[i] += dsp->int_i[i - 1];
-			dsp->int_q[i] += dsp->int_q[i - 1];
-		}
+		p = dsp->comm_pos;
+		dsp->delay_i[p * P + dsp->delay_pos[p]] = mix_i;
+		dsp->delay_q[p * P + dsp->delay_pos[p]] = mix_q;
+		dsp->delay_pos[p] = (dsp->delay_pos[p] + 1) % P;
 
-		if (dsp->cic_counter == 0) {
-			cic_i = dsp->int_i[dsp->cic_stages - 1];
-			cic_q = dsp->int_q[dsp->cic_stages - 1];
+		dsp->comm_pos = (dsp->comm_pos + 1) % R;
 
-			for (i = 0; i < dsp->cic_stages; i++) {
-				double prev_i = dsp->comb_i[i];
-				double prev_q = dsp->comb_q[i];
-				dsp->comb_i[i] = cic_i;
-				dsp->comb_q[i] = cic_q;
-				cic_i = cic_i - prev_i;
-				cic_q = cic_q - prev_q;
+		if (dsp->comm_pos == 0) {
+			int ph;
+			double sum_i = 0.0, sum_q = 0.0;
+
+			for (ph = 0; ph < R; ph++) {
+				const double *c = dsp->coeffs + ph * P;
+				const double *di = dsp->delay_i + ph * P;
+				const double *dq = dsp->delay_q + ph * P;
+				int dp = dsp->delay_pos[ph];
+				int j;
+
+				for (j = 0; j < P; j++) {
+					int idx = (dp - 1 - j + P) % P;
+					sum_i += c[j] * di[idx];
+					sum_q += c[j] * dq[idx];
+				}
 			}
 
-			cic_i /= dsp->cic_gain;
-			cic_q /= dsp->cic_gain;
+			if (sum_i > 32767.0) sum_i = 32767.0;
+			if (sum_i < -32768.0) sum_i = -32768.0;
+			if (sum_q > 32767.0) sum_q = 32767.0;
+			if (sum_q < -32768.0) sum_q = -32768.0;
 
-			if (cic_i > 32767.0) cic_i = 32767.0;
-			if (cic_i < -32768.0) cic_i = -32768.0;
-			if (cic_q > 32767.0) cic_q = 32767.0;
-			if (cic_q < -32768.0) cic_q = -32768.0;
-
-			output[out_idx++] = (int16_t)cic_i;
-			output[out_idx++] = (int16_t)cic_q;
+			output[out_idx++] = (int16_t)sum_i;
+			output[out_idx++] = (int16_t)sum_q;
 		}
-
-		dsp->cic_counter++;
-		if (dsp->cic_counter >= dsp->decimation)
-			dsp->cic_counter = 0;
 	}
 
 	*output_len = out_idx;
@@ -157,12 +227,15 @@ int rtlsdr_dsp_process(struct rtlsdr_dsp *dsp,
 
 void rtlsdr_dsp_reset(struct rtlsdr_dsp *dsp)
 {
-	if (dsp) {
-		dsp->phase = 0.0;
-		dsp->cic_counter = 0;
-		memset(dsp->int_i, 0, dsp->cic_stages * sizeof(double));
-		memset(dsp->int_q, 0, dsp->cic_stages * sizeof(double));
-		memset(dsp->comb_i, 0, dsp->cic_stages * sizeof(double));
-		memset(dsp->comb_q, 0, dsp->cic_stages * sizeof(double));
-	}
+	int i;
+
+	if (!dsp)
+		return;
+
+	dsp->phase = 0.0;
+	dsp->comm_pos = 0;
+	for (i = 0; i < dsp->num_phases; i++)
+		dsp->delay_pos[i] = 0;
+	memset(dsp->delay_i, 0, dsp->num_phases * dsp->taps_per_phase * sizeof(double));
+	memset(dsp->delay_q, 0, dsp->num_phases * dsp->taps_per_phase * sizeof(double));
 }
